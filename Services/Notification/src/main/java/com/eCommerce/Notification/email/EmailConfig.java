0ab6594afa36b284a0f1cb2e4aa59e46358d1e08
hemanth7301 <saihemanthsakhamuri@gmail.com>
2024-06-22 12:33:32 +0530

    Added Email Config

---
+++ Services/Notification/src/main/java/com/eCommerce/Notification/email/EmailConfig.java
@@ -0,0 +1,28 @@
+package com.eCommerce.Notification.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("localhost");
+        mailSender.setPort(1025);
+        mailSender.setUsername("saihemanthsakhamuri@gmail.com");
+        mailSender.setPassword("admin");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
